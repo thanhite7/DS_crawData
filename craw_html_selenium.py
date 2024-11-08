4541240b7b7b1c10d99e169bb32993a36ba5cc0e
thanhite7 <thanhnguyentienusd@gmail.com>
2024-11-09 02:16:33 +0700

    craw data

--- craw_html_selenium.py
+++ craw_html_selenium.py
@@ -17,11 +17,6 @@ chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")  
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-javascript") 
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-software-rasterizer")
-chrome_options.add_argument("--disable-features=VizDisplayCompositor")
-chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument("--disable-infobars")
 
 
 driver = webdriver.Chrome(options=chrome_options)
